72f173373dc8c761f20e297c44a1c3a76a1967ed
Damien PELISSIER <damien.pelissier@telecom-paris.fr>
2024-10-03 00:47:41 +0200

    \display_func.py: Modification pour pouvoir afficher une seule image

--- display_func.py
+++ display_func.py
@@ -2,22 +2,32 @@ import matplotlib.pyplot as plt
 
 
 def show_image(img1, img1_title='Original Image', is_im1_grey=False, img2=None, img2_title='Filtered Image', is_im2_grey=False):
-    fig, (ax1, ax2) = plt.subplots(
-        1, 2, figsize=(10, 5), sharex=True, sharey=True)
-    ax1.axis('off')
-    ax2.axis('off')
-
-    # Display the original and the filtered image
-    if is_im1_grey:
-        ax1.imshow(
-            img1, cmap='gray'), ax1.set_title(img1_title)
-    else:
-        ax1.imshow(img1), ax1.set_title(img1_title)
     if img2 is not None:
-        if is_im2_grey:
-            ax2.imshow(img2, cmap='gray'), ax2.set_title(
-                img2_title)
+        fig, (ax1, ax2) = plt.subplots(
+            1, 2, figsize=(10, 5), sharex=True, sharey=True)
+        ax1.axis('off')
+        ax2.axis('off')
+
+        # Display the original and the filtered image
+        ax1.set_title(img1_title)
+        if is_im1_grey:
+            ax1.imshow(
+                img1, cmap='gray')
         else:
-            ax2.imshow(img2), ax2.set_title(img2_title)
+            ax1.imshow(img1)
+
+        ax2.set_title(img2_title)
+        if is_im2_grey:
+            ax2.imshow(img2, cmap='gray')
+        else:
+            ax2.imshow(img2)
+
+    else:
+        plt.title(img1_title)
+        plt.axis('off')
+        if is_im1_grey:
+            plt.imshow(img1, cmap='gray')
+        else:
+            plt.imshow(img1)
 
     plt.show()
